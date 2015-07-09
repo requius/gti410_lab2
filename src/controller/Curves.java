34738d15d4503e1aaa1565b61a78267c1356d9d8
aj89610 <aj89610@10.196.122.181>
2015-07-09 14:37:51 -0400

    Ajout HermiteCurveType et BSplineCurveType

--- src/controller/Curves.java
+++ src/controller/Curves.java
@@ -19,21 +19,22 @@ import java.util.List;
 
 import view.Application;
 import view.CurvesPanel;
-
+import model.BSplineCurveType;
 import model.BezierCurveType;
 import model.ControlPoint;
 import model.Curve;
 import model.CurvesModel;
 import model.DocObserver;
 import model.Document;
+import model.HermiteCurveType;
 import model.PolylineCurveType;
 import model.Shape;
 
 /**
  * <p>Title: Curves</p>
  * <p>Description: (AbstractTransformer)</p>
- * <p>Copyright: Copyright (c) 2004 Sébastien Bois, Eric Paquette</p>
- * <p>Company: (ÉTS) - École de Technologie Supérieure</p>
+ * <p>Copyright: Copyright (c) 2004 Sebastien Bois, Eric Paquette</p>
+ * <p>Company: (ETS) - Ecole de Technologie Superieure</p>
  * @author unascribed
  * @version $Revision: 1.9 $
  */
@@ -105,6 +106,10 @@ public class Curves extends AbstractTransformer implements DocObserver {
 			curve.setCurveType(new BezierCurveType(CurvesModel.BEZIER));
 		} else if (string == CurvesModel.LINEAR) {
 			curve.setCurveType(new PolylineCurveType(CurvesModel.LINEAR));
+		} else if (string == CurvesModel.HERMITE) {
+			curve.setCurveType(new HermiteCurveType(CurvesModel.HERMITE));
+		} else if (string == CurvesModel.BSPLINE) {
+			curve.setCurveType(new BSplineCurveType(CurvesModel.BSPLINE));	
 		} else {
 			System.out.println("Curve type [" + string + "] is unknown.");
 		}
